a17c6b83df3826066f934d42333924f963826d1d
Gigi-U <andanlibres@gmail.com>
2023-11-20 20:37:53 -0300

    clase 11

--- internal/products/service.go
+++ internal/products/service.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"log"
 
-	"github.com/Gigi-U/PRODUCTS-CRUD-GO.git/internal/domain"
+	"github.com/Gigi-U/PRODUCTS-CRUD-GO.git/internal/models"
 )
 
 type Service interface {
-	Create(ctx context.Context, producto domain.Producto) (domain.Producto, error)
-	GetAll(ctx context.Context) ([]domain.Producto, error)
-	GetByID(ctx context.Context, id string) (domain.Producto, error)
-	Update(ctx context.Context, producto domain.Producto, id string) (domain.Producto, error)
+	Create(ctx context.Context, producto models.Producto) (models.Producto, error)
+	GetAll(ctx context.Context) ([]models.Producto, error)
+	GetByID(ctx context.Context, id string) (models.Producto, error)
+	Update(ctx context.Context, producto models.Producto, id string) (models.Producto, error)
 	Delete(ctx context.Context, id string) error
 }
 
@@ -24,44 +24,44 @@ func NewServiceProduct(repository Repository) Service {
 }
 
 // Create ....
-func (s *service) Create(ctx context.Context, producto domain.Producto) (domain.Producto, error) {
+func (s *service) Create(ctx context.Context, producto models.Producto) (models.Producto, error) {
 	producto, err := s.repository.Create(ctx, producto)
 	if err != nil {
 		log.Println("[ProductsService][Create] error creating product", err)
-		return domain.Producto{}, err
+		return models.Producto{}, err
 	}
 
 	return producto, nil
 }
 
 // GetAll ...
-func (s *service) GetAll(ctx context.Context) ([]domain.Producto, error) {
+func (s *service) GetAll(ctx context.Context) ([]models.Producto, error) {
 	listProducts, err := s.repository.GetAll(ctx)
 	if err != nil {
 		log.Println("[ProductsService][GetAll] error getting all products", err)
-		return []domain.Producto{}, err
+		return []models.Producto{}, err
 	}
 
 	return listProducts, nil
 }
 
 // GetByID ....
-func (s *service) GetByID(ctx context.Context, id string) (domain.Producto, error) {
+func (s *service) GetByID(ctx context.Context, id string) (models.Producto, error) {
 	producto, err := s.repository.GetByID(ctx, id)
 	if err != nil {
 		log.Println("[ProductsService][GetByID] error getting product by ID", err)
-		return domain.Producto{}, err
+		return models.Producto{}, err
 	}
 
 	return producto, nil
 }
 
 // Update ...
-func (s *service) Update(ctx context.Context, producto domain.Producto, id string) (domain.Producto, error) {
+func (s *service) Update(ctx context.Context, producto models.Producto, id string) (models.Producto, error) {
 	producto, err := s.repository.Update(ctx, producto, id)
 	if err != nil {
 		log.Println("[ProductsService][Update] error updating product by ID", err)
-		return domain.Producto{}, err
+		return models.Producto{}, err
 	}
 
 	return producto, nil
